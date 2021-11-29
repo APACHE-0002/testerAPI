a8fea85653b99f81f6af616fa9c263c34717d4bf
APACHE-0002 <48190578+APACHE-0002@users.noreply.github.com>
2021-11-29 15:46:34 -0300

    all

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("../index");
 
+//caso a variavel cliente tenha da um valor que 
+//nao encaixe nos parametros dos testes ira retornar erro
 
 describe("GET /", () => {
     it("responds with Hello World!", (done) => {
